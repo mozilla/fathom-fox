cd60425599cbf97b4064828b21a37bedff7645c2
Erik Rose <erik@mozilla.com>
2018-01-12 12:56:40 -0500

    Add auto-recompiling and reloading.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
+const WebpackWebExt = require('webpack-webext-plugin');
+
+
 module.exports = {
     entry: {
         background_scripts: "./background_scripts/background.js",
@@ -8,5 +11,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "addon"),
         filename: "[name]/index.js"
-    }
+    },
+    plugins: [
+      new WebpackWebExt({
+        runOnce: false,
+        argv: ['lint', '-s', 'addon/'],
+      }),
+
+      new WebpackWebExt({
+        runOnce: true,
+        maxRetries: 3,
+        argv: ['run', '-s', 'addon/', '--firefox', 'nightly'],
+      })
+    ]
 };
