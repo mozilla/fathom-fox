ef81ab9b0350e8dd2226517207bb1f09070bebb0
Erik Rose <erik@mozilla.com>
2018-01-12 12:11:59 -0500

    Clear out stuff I don't need. Add a license file.

--- popup/left-pad.js
+++ popup/left-pad.js
@@ -1,18 +1,19 @@
 const leftPad = require("left-pad");
 
-const resultNode = document.getElementById("result");
-const textNode = document.getElementById("text");
-const amountNode = document.getElementById("amount");
-const withNode = document.getElementById("with");
+const el = document.getElementById.bind(document);
+const resultNode = el("result");
+const textNode = el("text");
+const amountNode = el("amount");
+const withNode = el("with");
 
-document.getElementById("leftpad-form").addEventListener("submit", (e) => {
+el("leftpad-form").addEventListener("submit", (e) => {
     e.preventDefault();
 
     console.log("padding");
     resultNode.value = leftPad(textNode.value, amountNode.valueAsNumber, withNode.value);
 }, false);
 
-document.getElementById("pad-bg").addEventListener("click", (e) => {
+el("pad-bg").addEventListener("click", (e) => {
     var sendingMessage = browser.runtime.sendMessage({
         text: textNode.value,
         amount: amountNode.valueAsNumber,
