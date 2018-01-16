a07b1d8f3407b66841764d23ab538e8c70e75886
Erik Rose <erik@mozilla.com>
2018-01-16 15:22:31 -0500

    Get an answer back from the content script.
    We can't just return a value that pops out of executeScript() because webpack wraps our content script in a function. Thus, we can't get it to emit a statement on the top level, which is where executeScript() looks for a return value. Perhaps we could twiddle a webpack flag, but mucking with webpack tends to be a time sink.

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -1,7 +1,14 @@
 async function go() {
-    browser.tabs.executeScript({
-        file: '/content_scripts/index.js'
-    });
+    const tab = await browser.tabs.create({url: 'http://www.grinchcentral.com/', active: true});
+    // Fire this and forget. We then pick up processing when the content script sends us a message, in receiveMessage().
+    browser.tabs.executeScript(tab.id, {file: '/content_scripts/index.js'})
+                .catch((err) => console.log(`Error while freeze-drying: ${err}`));
 }
-
 browser.browserAction.onClicked.addListener(go);
+
+function receiveMessage(message) {
+    console.log('received:' + message);
+}
+browser.runtime.onMessage.addListener(receiveMessage);
+
+// We've proven we can console.log from a promise then() callback.
