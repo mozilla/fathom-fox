6b7b77b1630ad4d3f0ce7122aabaf860a015925d
Erik Rose <erik@mozilla.com>
2018-01-17 16:35:04 -0500

    Standardize the viewport size before freezing.
    Also start using Slashdot, a more challenging size with iframes and more images.

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -2,12 +2,28 @@
  * Handle clicks from the toolbar button.
  */
 async function go() {
-    const tab = await browser.tabs.create({url: 'http://www.grinchcentral.com/', active: true});
-    // Fire this and forget. We then pick up processing when the content script sends us a message, in saveHtml().
+    await setViewportSize(1024, 768);
+    const tab = await browser.tabs.create({url: 'http://www.slashdot.org/', active: true});
+    // Fire this and forget. We then pick up processing when the content script
+    // sends us a message, in saveHtml().
     browser.tabs.executeScript(tab.id, {file: '/content_scripts/index.js'})
                 .catch((err) => console.log(`Error while freeze-drying: ${err}`));
 }
 browser.browserAction.onClicked.addListener(go);
+
+/**
+ * Set the current window's size such that the content area is the size you
+ * pass in.
+ *
+ * @return a Promise that is resolved when the window size has been changed
+ */
+async function setViewportSize(width, height) {
+    const window = await browser.windows.getCurrent();
+    // Toolbars on the Mac on a fresh profile are 74px tall. We add that to get
+    // the content area to the given height. It would be lovely to not hard-
+    // code this.
+    return browser.windows.update(window.id, {width, height: height + 74});
+}
 
 /**
  * Receive the serialized HTML from the content script, and save it to disk.
