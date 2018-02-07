e1e6c8a2ed61e8297e000348d26d73a67ec0ffbb
Erik Rose <erik@mozilla.com>
2018-02-07 10:15:05 -0500

    Log the ID of the inspected element. Remove the rest of the example code.

--- addon/devtoolsOpener.js
+++ addon/devtoolsOpener.js
@@ -1,24 +1,5 @@
-/**
-This script is run whenever the devtools are open.
-In here, we can create our panel.
-*/
-
-function handleShown() {
-  console.log("panel is being shown");
-}
-
-function handleHidden() {
-  console.log("panel is being hidden");
-}
-
-/**
-Create a panel, and add listeners for panel show/hide events.
-*/
 browser.devtools.panels.create(
-  "Fathom",
-  "/icons/leftpad-32.png",
-  "/pages/devtoolsPanel.html"
-).then((newPanel) => {
-  newPanel.onShown.addListener(handleShown);
-  newPanel.onHidden.addListener(handleHidden);
-}); 
+  'Fathom',
+  '/icons/leftpad-32.png',
+  '/pages/devtoolsPanel.html'
+);
