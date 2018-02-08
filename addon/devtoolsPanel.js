624bc367e218eb2d150adda8f4f78dbc4fa2859d
Erik Rose <erik@mozilla.com>
2018-02-08 17:17:26 -0500

    Add a demo init-panel routine, and refactor to hide the ugliness of the eval() API's error handling a bit better.
    Committing this before I try a content script and messaging to get around the JSON-only barrier.

--- addon/devtoolsPanel.js
+++ addon/devtoolsPanel.js
@@ -1,15 +1,18 @@
 /**
-Handle errors from the injected script.
-Errors may come from evaluating the JavaScript itself
-or from the devtools framework.
-See https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/devtools.inspectedWindow/eval#Return_value
-*/
-function handleError(error) {
-  if (error.isError) {
-    console.log(`Devtools error: ${error.code}`);
-  } else {
-    console.log(`JavaScript error: ${error.value}`);
-  }
+ * Return whether the result from an inspectedWindow.eval() call was an error.
+ * Log it if it was.
+ */
+function isError(result) {
+    if (result[0] === undefined) {
+        const error = result[1];
+        if (error.isError) {
+            console.log(`Devtools error: ${error.code}`);
+        } else {
+            console.log(`JavaScript error: ${error.value}`);
+        }
+        return true;
+    }
+    return false;
 }
 
 /**
@@ -18,11 +21,9 @@ Log the result of the test, or
 if there was an error, call handleError.
 */
 function handlejQueryResult(result) {
-  if (result[0] !== undefined) {
-    console.log(`id: ${result[0]}`);
-  } else if (result[1]) {
-    handleError(result[1]);
-  }
+    if (!isError(result)) {
+        console.log(`id: ${result[0]}`);
+    }
 }
 /**
 When the user clicks the 'jquery' button,
@@ -33,3 +34,20 @@ document.getElementById('button_jquery').addEventListener('click', () => {
   browser.devtools.inspectedWindow.eval(checkjQuery)
     .then(handlejQueryResult);
 });
+
+/**
+ * Update the GUI to reflect the currently inspected page the first time the
+ * panel loads.
+ *
+ * When you navigate to a new page, the Console pane comes forward, so this re-
+ * runs when the Fathom pane is brought forward again.
+ */
+async function initPanel() {
+    const result = await browser.devtools.inspectedWindow.eval(
+        'document.querySelectorAll("[data-fathom]")[0].getAttribute("data-fathom")');
+    if (!isError(result)) {
+        console.log(result[0]);
+    }
+    // I don't seem able to pass DOM elements from the eval() context back into the panel. Simple values are fine. The docs, in fact, say only JSON values can be passed.
+}
+initPanel();
