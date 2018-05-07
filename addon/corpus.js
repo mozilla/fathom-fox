01c6764c03a0bec7f8e7cec93b11ae6d6e6c7ae3
Erik Rose <erik@mozilla.com>
2018-05-07 16:55:04 -0400

    This works.

--- addon/corpus.js
+++ addon/corpus.js
@@ -13,6 +13,7 @@ async function freezeAllPages() {
     // the window.
     const windowId = (await browser.windows.create({url: '/pages/blank.html'})).id;
     const blankTab = (await browser.tabs.query({windowId}))[0];
+    await tabCompletion(blankTab);
     await setViewportSize(blankTab, 1024, 768);
 
     // Freeze the pages:
@@ -35,6 +36,41 @@ async function freezeAllPages() {
 }
 document.getElementById('freeze').onclick = freezeAllPages;
 
+async function tabCompletion(tab) {
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    await sleep(2000);
+    return;
+    if (tab.status === 'loading') {
+        console.log('still loading');
+        return new Promise((resolve, reject) => {
+            const timer = setTimeout(
+                function giveUp() {
+                    browser.tabs.onUpdated.removeListener(onUpdated);
+                    if (tab.status === 'complete') {
+                        // Give it one last chance to dodge race condition
+                        // in which it completes between the initial test
+                        // and installation of the update listener.
+                        resolve(tab);
+                    } else {
+                        reject(new Error('Tab never reached the "complete" state.'));
+                    }
+                },
+                5000);
+            function onUpdated(tabId, changeInfo, updatedTab) {
+                if (tabId === tab.id && updatedTab.status === 'complete') {
+                    clearTimeout(timer);
+                    browser.tabs.onUpdated.removeListener(onUpdated);
+                    resolve(updatedTab);
+                }
+            }
+            browser.tabs.onUpdated.addListener(onUpdated);
+        });
+    }
+    console.log('not still loading');
+}
+
 /**
  * Serialize and download a page.
  *
@@ -44,6 +80,7 @@ document.getElementById('freeze').onclick = freezeAllPages;
  */
 async function freezePage(url, windowId, freezeOptions) {
     const tab = await browser.tabs.create({url, windowId, active: true});
+    await tabCompletion(tab);
     // Can't get a return value out of the content script because webpack wraps
     // our top-level stuff in a function. Instead, we use messaging.
     await browser.tabs.executeScript(tab.id, {file: '/contentScript.js'});
