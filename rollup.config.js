1a0f9f294ac10a604e8d87b189224293db812b38
Erik Rose <erik@mozilla.com>
2018-06-18 16:32:31 -0400

    It compiles without error now.
    (rollup -c, that is.)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import json from 'rollup-plugin-json';
-
+import builtins from 'rollup-plugin-node-builtins';
+import globals from 'rollup-plugin-node-globals';
 
 /**
  * Return typical rollup settings for a file of a given name.
@@ -15,13 +16,15 @@ function mindlesslyFactoredOutSettings(name) {
             name  // Convention: name the var the same thing.
         },
         plugins: [
-            resolve(),
+            resolve({preferBuiltins: true}),
             commonjs({
                 namedExports: {
                     'wu': ['forEach', 'map', 'flatten']
                 }
             }),
-            json()
+            json(),
+            globals(),
+            builtins()
         ]
     }
 }
