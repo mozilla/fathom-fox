4e1c335196c4eb60aa6def4629d0ff40d171f89d
Byron Jones <byron@glob.com.au>
2018-09-12 00:01:03 +0800

    revert revokeObjectURL logic

--- addon/download.js
+++ addon/download.js
@@ -5,21 +5,15 @@ async function download(html, options = {}) {
     const blob = new Blob([html], {type: 'text/html'});
     const url = URL.createObjectURL(blob);
 
-    // Revoke the url object when the download has completed.
-    function handleChanged(delta) {
-        if (delta.state && delta.state.current === "complete") {
-            URL.revokeObjectURL(url);
-            browser.downloads.onChanged.removeListener(handleChanged);
-        }
-    }
-    browser.downloads.onChanged.addListener(handleChanged);
-
     // Save html using the specified filename as a template.
     let downloadId = await browser.downloads.download({
         url,
         filename: options.filename || 'Untitled.html',
         saveAs: options.saveAs || false,
     });
+
+    // Give it 10 seconds; FF can be a bit slow.
+    window.setTimeout(() => URL.revokeObjectURL(url), 1000 * 10);
 
     // Return the basename of the chosen filename.
     let filename = (await browser.downloads.search({id: downloadId}))[0].filename;
