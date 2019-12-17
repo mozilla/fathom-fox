cee99d8c3fe7eec926881f612e04fa630c54ab8f
Erik Rose <erik@mozilla.com>
2019-12-17 16:18:16 -0500

    Remove the duplicates of things we've since lifted into Fathom proper.

--- src/trainees.js
+++ src/trainees.js
@@ -1,5 +1,5 @@
 import {ruleset, rule, dom, type, score, out} from 'fathom-web';
-import {ancestors} from 'fathom-web/utilsForFrontend';
+import {ancestors, isVisible, linearScale, rgbaFromString, saturation} from 'fathom-web/utilsForFrontend';
 
 
 /**
@@ -54,7 +54,7 @@ trainees.set(
                 const rect = fnode.element.getBoundingClientRect();
                 const hDifference = Math.abs(rect.height - window.innerHeight);
                 const wDifference = Math.abs(rect.width - window.innerWidth);
-                return trapezoid(hDifference + wDifference, 250, 0);  // 250px is getting into "too tall to just be nav or something" territory.
+                return linearScale(hDifference + wDifference, 250, 0);  // 250px is getting into "too tall to just be nav or something" territory.
             }
 
             /**
@@ -72,7 +72,7 @@ trainees.set(
                 if (totalOpacity === 1) {  // seems to work even though a float
                     ret = 0;
                 } else {
-                    ret = trapezoid(totalOpacity, .4, .6);
+                    ret = linearScale(totalOpacity, .4, .6);
                 }
                 return ret;
             }
@@ -82,7 +82,7 @@ trainees.set(
              */
             function monochrome(fnode) {
                 const rgba = rgbaFromString(getComputedStyle(fnode.element).getPropertyValue('background-color'));
-                return trapezoid(1 - saturation(...rgba), .96, 1);
+                return linearScale(1 - saturation(...rgba), .96, 1);
             }
 
             function suspiciousClassOrId(fnode) {
@@ -112,86 +112,6 @@ trainees.set(
                 return (-(.3 ** (numOccurences + .1685)) + 1);
             }
 
-            /**
-             * Score hidden things real low.
-             *
-             * For training, this avoids false failures (and thus gives us more
-             * accurate accuracy numbers) since some pages have multiple
-             * popups, all but one of which are hidden in our captures.
-             * However, for actual use, consider dropping this rule, since
-             * deleting popups before they pop up may not be a bad thing.
-             */
-            function visible(fnode) {
-                const element = fnode.element;
-                for (const ancestor of ancestors(element)) {
-                    const style = getComputedStyle(ancestor);
-                    if (style.getPropertyValue('visibility') === 'hidden' ||
-                        style.getPropertyValue('display') === 'none') {
-                        return 0;
-                    }
-                    // Could add opacity and size checks here, but the
-                    // "nearlyOpaque" and "big" rules already deal with opacity
-                    // and size. If they don't do their jobs, maybe repeat
-                    // their work here (so it gets a different coefficient).
-                }
-                return 1;
-            }
-
-            /* Utility procedures */
-
-            /**
-             * Return the extracted [r, g, b, a] values from a string like "rgba(0, 5, 255, 0.8)",
-             * and scale them to 0..1. If no alpha is specified, return undefined for it.
-             */
-            function rgbaFromString(str) {
-                const m = str.match(/^rgba?\s*\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*(?:,\s*(\d+(?:\.\d+)?)\s*)?\)$/i);
-                if (m) {
-                    return [m[1] / 255, m[2] / 255, m[3] / 255, m[4] === undefined ? undefined : parseFloat(m[4])];
-                } else {
-                    throw new Error("Color " + str + " did not match pattern rgb[a](r, g, b[, a]).");
-                }
-            }
-
-            /**
-             * Scale a number to the range [0, 1].
-             *
-             * For a rising trapezoid, the result is 0 until the input reaches
-             * zeroAt, then increases linearly until oneAt, at which it becomes
-             * 1. To make a falling trapezoid, where the result is 1 to the
-             * left and 0 to the right, use a zeroAt greater than oneAt.
-             */
-            function trapezoid(number, zeroAt, oneAt) {
-                const isRising = zeroAt < oneAt;
-                if (isRising) {
-                    if (number <= zeroAt) {
-                        return 0;
-                    } else if (number >= oneAt) {
-                        return 1;
-                    }
-                } else {
-                    if (number >= zeroAt) {
-                        return 0;
-                    } else if (number <= oneAt) {
-                        return 1;
-                    }
-                }
-                const slope = 1 / (oneAt - zeroAt);
-                return slope * (number - zeroAt);
-            }
-
-            /**
-             * Return the saturation 0..1 of a color defined by RGB values 0..1.
-             */
-            function saturation(r, g, b) {
-                const cMax = Math.max(r, g, b);
-                const cMin = Math.min(r, g, b);
-                const delta = cMax - cMin;
-                const lightness = (cMax + cMin) / 2;
-                const denom = (1 - (Math.abs(2 * lightness - 1)));
-                // Return 0 if it's black (R, G, and B all 0).
-                return (denom === 0) ? 0 : delta / denom;
-            }
-
             /* The actual ruleset */
 
             const rules = ruleset([
@@ -200,7 +120,7 @@ trainees.set(
                 rule(type('overlay'), score(nearlyOpaque), {name: 'nearlyOpaque'}),
                 rule(type('overlay'), score(monochrome), {name: 'monochrome'}),
                 rule(type('overlay'), score(suspiciousClassOrId), {name: 'classOrId'}),
-                rule(type('overlay'), score(visible), {name: 'visible'}),
+                rule(type('overlay'), score(isVisible), {name: 'visible'}),
                 rule(type('overlay').max(), out('overlay'))
             ]);
             return rules;
